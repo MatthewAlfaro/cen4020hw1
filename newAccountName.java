df5368fc74b2c14756a32ddda57a1b2850240a31
MatthewAlfaro <157857628+MatthewAlfaro@users.noreply.github.com>
2024-01-26 22:19:24 -0500

    Update and rename AccountName.java to newAccountName.java

--- newAccountName.java
+++ newAccountName.java
@@ -1,8 +1,9 @@
 //Matthew Alfaro
 //CEN4020 Assignment1 - GitHub
 
-public class AccountName {
+public class newAccountName {
     public static void main(String[] args) {
         System.out.println("My GitHub account name is: MatthewAlfaro");
+        System.out.println("The name of the repository is: cen4020hw1");
     }
 }
