f7521bb9166fa34f2bc8638259b4da12760b4403
MatthewAlfaro <mattalfaro19@gmail.com>
2024-01-26 21:15:29 -0500

    added the java file

---
+++ gitHub.java
@@ -0,0 +1,8 @@
+//Matthew Alfaro
+//CEN4020 Assignment1 - GitHub
+
+public class gitHub {
+    public static void main(String[] args) {
+        System.out.print("My GitHub account name is: MatthewAlfaro");
+    }
+}
