e6317e2c8d70efb97608cffbf145a276a829bfe9
MatthewAlfaro <157857628+MatthewAlfaro@users.noreply.github.com>
2024-01-26 21:46:44 -0500

    Delete nameAndRepository.java

--- nameAndRepository.java
+++
@@ -1,8 +0,0 @@
-//Matthew Alfaro
-////CEN4020 Assignment1 - GitHub
-public class nameAndRepository {
-	public static void main(String[] args) {
-		System.out.println("My GitHub account name is: MatthewAlfaro");
-		System.out.println("The repository name is: cen4020hw1");
-	}
-}	
